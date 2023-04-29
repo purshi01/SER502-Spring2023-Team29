c6f9decccee4879f77ecebc1b5d3c21d6aa10225
purshi01 <purushothamapurshi8@gmail.com>
2023-04-28 19:00:29 -0700

    Initial commit for run time file which is contains the evaluations of the tokens

--- src/Runtime/run_time.pl
+++ src/Runtime/run_time.pl
@@ -1,2 +1,12 @@
-print("Hello, World!")
-print("Testing the commit")
+%----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+% Evaluate Command
+eval_command_s(t_command(), Env_s, Env_s).
+eval_command_s(t_command(X_s, Y_s), Env_s, FinalEnv_s) :- eval_statement(X_s, Env_s, Env1_s), eval_command_s(Y_s, Env1_s, FinalEnv_s).
+
+%----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+% Evaluate Block
+eval_block(t_block(X_s), Env_s, FinalEnv_s):- eval_command_s(X_s, Env_s, FinalEnv_s).
+
+program_eval(t_program(X_s), Env_s):- eval_block(X_s, [], Env_s).
